05ac238b5adcd58db510a164b6032a8d8d10f51d
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-03-26 04:41:13 +0900

    fix /delete-user /register-user

--- bot/dist/commands/utilities/user.js
+++ bot/dist/commands/utilities/user.js
@@ -15,10 +15,29 @@ const registerUser = {
     data: new discord_js_1.SlashCommandBuilder().setName('register-user').setDescription('Sample slash command.'),
     async execute(interaction) {
         if (guildId === interaction.guild?.id) {
-            await interaction.reply(`[OAuth URL](${registerUrl})`);
+            const registerUserLinkButton = new discord_js_1.ButtonBuilder()
+                .setLabel('Register Your Account')
+                .setURL(registerUrl)
+                .setStyle(discord_js_1.ButtonStyle.Link);
+            const row = new discord_js_1.ActionRowBuilder().addComponents(registerUserLinkButton);
+            const response = await interaction.reply({
+                content: '以下のボタンをクリックしてユーザーの登録絵をしてください。',
+                components: [row],
+            });
+            //スラッシュコマンドを入力したユーザーのみがコンポーネントの操作ができる
+            const collectorFilter = (i) => i.user.id === interaction.user.id;
+            try {
+                await response.awaitMessageComponent({ filter: collectorFilter, time: 5_000 });
+            }
+            catch (error) {
+                await interaction.editReply({
+                    content: '前の操作をしてから5秒以上が経過したため、ボタンを削除しました。',
+                    components: [],
+                });
+            }
         }
         else {
-            await interaction.reply('You are not guild member');
+            await interaction.reply('あなたは、ギルドメンバーではありません。');
         }
     },
 };
@@ -27,10 +46,45 @@ const deleteUser = {
     data: new discord_js_1.SlashCommandBuilder().setName('delete-user').setDescription('Sample slash command.'),
     async execute(interaction) {
         if (guildId === interaction.guild?.id) {
-            await interaction.reply(`[OAuth URL](${deleteUrl})`);
+            const acceptButton = new discord_js_1.ButtonBuilder().setCustomId('accept').setLabel('Yes').setStyle(discord_js_1.ButtonStyle.Danger);
+            const rejectButton = new discord_js_1.ButtonBuilder().setCustomId('reject').setLabel('No').setStyle(discord_js_1.ButtonStyle.Success);
+            const firstRow = new discord_js_1.ActionRowBuilder().addComponents(acceptButton, rejectButton);
+            const deleteUserLinkButton = new discord_js_1.ButtonBuilder()
+                .setLabel('Delete Your Account')
+                .setURL(deleteUrl)
+                .setStyle(discord_js_1.ButtonStyle.Link);
+            const secondRow = new discord_js_1.ActionRowBuilder().addComponents(deleteUserLinkButton);
+            const response = await interaction.reply({
+                content: 'あなたが関係している借金の情報もすべて消えますが、よろしいですか。',
+                components: [firstRow],
+            });
+            //スラッシュコマンドを入力したユーザーのみがコンポーネントの操作ができる
+            const collectorFilter = (i) => i.user.id === interaction.user.id;
+            try {
+                const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 5_000 });
+                if (confirmation.customId === 'accept') {
+                    const Linkconfirmation = await confirmation.update({
+                        content: `以下のボタンをクリックしてユーザーの削除を行ってください。`,
+                        components: [secondRow],
+                    });
+                    await Linkconfirmation.awaitMessageComponent({
+                        filter: collectorFilter,
+                        time: 5_000,
+                    });
+                }
+                if (confirmation.customId === 'reject') {
+                    await confirmation.update({ content: 'ユーザーの削除は行われませんでした。', components: [] });
+                }
+            }
+            catch (error) {
+                await interaction.editReply({
+                    content: '前の操作をしてから5秒以上が経過したため、ボタンを削除しました。',
+                    components: [],
+                });
+            }
         }
         else {
-            await interaction.reply('You are not guild member');
+            await interaction.reply('あなたは、ギルドメンバーではありません。');
         }
     },
 };
--- bot/src/commands/utilities/user.ts
+++ bot/src/commands/utilities/user.ts
@@ -1,4 +1,17 @@
-import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
+    CacheType,
+    ChannelSelectMenuInteraction,
+    CommandInteraction,
+    MentionableSelectMenuInteraction,
+    RoleSelectMenuInteraction,
+    SlashCommandBuilder,
+    StringSelectMenuInteraction,
+    UserSelectMenuInteraction,
+} from 'discord.js';
 import dotenv from 'dotenv';
 import { checkIsString } from '../../types';
 
@@ -12,9 +25,38 @@ const registerUser = {
     data: new SlashCommandBuilder().setName('register-user').setDescription('Sample slash command.'),
     async execute(interaction: CommandInteraction) {
         if (guildId === interaction.guild?.id) {
-            await interaction.reply(`[OAuth URL](${registerUrl})`);
+            const registerUserLinkButton = new ButtonBuilder()
+                .setLabel('Register Your Account')
+                .setURL(registerUrl)
+                .setStyle(ButtonStyle.Link);
+            const row = new ActionRowBuilder<ButtonBuilder>().addComponents(registerUserLinkButton);
+
+            const response = await interaction.reply({
+                content: '以下のボタンをクリックしてユーザーの登録絵をしてください。',
+                components: [row],
+            });
+
+            //スラッシュコマンドを入力したユーザーのみがコンポーネントの操作ができる
+            const collectorFilter = (
+                i:
+                    | ButtonInteraction<CacheType>
+                    | StringSelectMenuInteraction<CacheType>
+                    | UserSelectMenuInteraction<CacheType>
+                    | RoleSelectMenuInteraction<CacheType>
+                    | MentionableSelectMenuInteraction<CacheType>
+                    | ChannelSelectMenuInteraction<CacheType>
+            ) => i.user.id === interaction.user.id;
+
+            try {
+                await response.awaitMessageComponent({ filter: collectorFilter, time: 5_000 });
+            } catch (error) {
+                await interaction.editReply({
+                    content: '前の操作をしてから5秒以上が経過したため、ボタンを削除しました。',
+                    components: [],
+                });
+            }
         } else {
-            await interaction.reply('You are not guild member');
+            await interaction.reply('あなたは、ギルドメンバーではありません。');
         }
     },
 };
@@ -23,9 +65,54 @@ const deleteUser = {
     data: new SlashCommandBuilder().setName('delete-user').setDescription('Sample slash command.'),
     async execute(interaction: CommandInteraction) {
         if (guildId === interaction.guild?.id) {
-            await interaction.reply(`[OAuth URL](${deleteUrl})`);
+            const acceptButton = new ButtonBuilder().setCustomId('accept').setLabel('Yes').setStyle(ButtonStyle.Danger);
+            const rejectButton = new ButtonBuilder().setCustomId('reject').setLabel('No').setStyle(ButtonStyle.Success);
+            const firstRow = new ActionRowBuilder<ButtonBuilder>().addComponents(acceptButton, rejectButton);
+
+            const deleteUserLinkButton = new ButtonBuilder()
+                .setLabel('Delete Your Account')
+                .setURL(deleteUrl)
+                .setStyle(ButtonStyle.Link);
+            const secondRow = new ActionRowBuilder<ButtonBuilder>().addComponents(deleteUserLinkButton);
+
+            const response = await interaction.reply({
+                content: 'あなたが関係している借金の情報もすべて消えますが、よろしいですか。',
+                components: [firstRow],
+            });
+            //スラッシュコマンドを入力したユーザーのみがコンポーネントの操作ができる
+            const collectorFilter = (
+                i:
+                    | ButtonInteraction<CacheType>
+                    | StringSelectMenuInteraction<CacheType>
+                    | UserSelectMenuInteraction<CacheType>
+                    | RoleSelectMenuInteraction<CacheType>
+                    | MentionableSelectMenuInteraction<CacheType>
+                    | ChannelSelectMenuInteraction<CacheType>
+            ) => i.user.id === interaction.user.id;
+
+            try {
+                const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 5_000 });
+                if (confirmation.customId === 'accept') {
+                    const Linkconfirmation = await confirmation.update({
+                        content: `以下のボタンをクリックしてユーザーの削除を行ってください。`,
+                        components: [secondRow],
+                    });
+                    await Linkconfirmation.awaitMessageComponent({
+                        filter: collectorFilter,
+                        time: 5_000,
+                    });
+                }
+                if (confirmation.customId === 'reject') {
+                    await confirmation.update({ content: 'ユーザーの削除は行われませんでした。', components: [] });
+                }
+            } catch (error) {
+                await interaction.editReply({
+                    content: '前の操作をしてから5秒以上が経過したため、ボタンを削除しました。',
+                    components: [],
+                });
+            }
         } else {
-            await interaction.reply('You are not guild member');
+            await interaction.reply('あなたは、ギルドメンバーではありません。');
         }
     },
 };
